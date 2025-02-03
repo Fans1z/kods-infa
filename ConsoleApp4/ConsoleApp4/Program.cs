a30a2ba4d9980e93330c5733812fbd35b8c100af
Fans1z <vitaka666@gmail.com>
2025-02-03 19:54:26 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите вероятность события в процентах (от 0 до 100):");
+        double probability = Convert.ToDouble(Console.ReadLine());
+
+        if (probability < 0 || probability > 100)
+        {
+            Console.WriteLine("Вероятность должна быть в диапазоне от 0 до 100.");
+            return;
+        }
+
+        bool eventOccurred = CheckEvent(probability);
+
+        if (eventOccurred)
+        {
+            Console.WriteLine("Событие произошло!");
+        }
+        else
+        {
+            Console.WriteLine("Событие не произошло.");
+        }
+    }
+
+    static bool CheckEvent(double probability)
+    {
+        Random random = new Random();
+        double randomValue = random.NextDouble() * 100;
+
+        return randomValue <= probability;
+    }
+}
---
+++ Второе задание/Второе задание/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array1 = GenerateRandomArray(20);
+        int[] array2 = GenerateRandomArray(20);
+
+        Array.Sort(array1);
+        Array.Sort(array2);
+
+        Console.WriteLine("Массив 1:");
+        PrintArray(array1);
+
+        Console.WriteLine("\nМассив 2:");
+        PrintArray(array2);
+
+        Console.WriteLine("\nОбщие элементы в обоих массивах:");
+        FindCommonElements(array1, array2);
+    }
+
+    static int[] GenerateRandomArray(int length)
+    {
+        Random random = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.Next(0, 101);
+        }
+        return array;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        Console.WriteLine(string.Join(", ", array));
+    }
+
+    static void FindCommonElements(int[] array1, int[] array2)
+    {
+        var commonElements = new System.Collections.Generic.HashSet<int>();
+
+        foreach (int num in array1)
+        {
+            if (BinarySearch(array2, num) && !commonElements.Contains(num))
+            {
+                commonElements.Add(num);
+                Console.Write(num + " ");
+            }
+        }
+
+        if (commonElements.Count == 0)
+        {
+            Console.WriteLine("Общих элементов нет.");
+        }
+    }
+
+    static bool BinarySearch(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+            {
+                return true;
+            }
+
+            if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
