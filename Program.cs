0432f29f66dcd43dbc6d3e30152661f6f0e0e666
Fans1z <vitaka666@gmail.com>
2025-02-03 21:31:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите вероятность события в процентах (от 0 до 100):");
+        int probability = int.Parse(Console.ReadLine());
+
+        if (CheckEvent(probability))
+        {
+            Console.WriteLine("Событие произошло!");
+        }
+        else
+        {
+            Console.WriteLine("Событие не произошло.");
+        }
+    }
+
+    static bool CheckEvent(int probability)
+    {
+        if (probability < 0 || probability > 100)
+        {
+            Console.WriteLine("Ошибка: вероятность должна быть от 0 до 100.");
+            return false;
+        }
+
+        Random random = new Random();
+        int randomValue = random.Next(0, 101);
+
+        return randomValue <= probability;
+    }
+}
